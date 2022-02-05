d22cd11e156c5012c96fda154181ae294b3d4f97
rjp301 <rileypaul96@gmail.com>
2022-02-04 23:40:41 -0700

    Add unit tests for helper functions

--- public/scripts/tileHelpers.js
+++ public/scripts/tileHelpers.js
@@ -1,10 +1,13 @@
 const tileLabel = (x, y) => {
-  const alpha = 'abcdefghijklmnopqrstuvwxyz';
-  return alpha[y].toUpperCase() + String(x + 1);
+  // 0-indexed position from top left
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const alpha = alphabet.charAt(y).toUpperCase();
+  const num = String(x + 1);
+  return  alpha + num;
 };
 
 const xyFromIndex = (index, dim = 10) => {
-  return (index % (dim + 1), Math.floor(index / (dim + 1)));
+  return [index % (dim + 1), Math.floor(index / (dim + 1))];
 };
 
 module.exports = { tileLabel, xyFromIndex };
--- test/tileHelpers.test.js
+++ test/tileHelpers.test.js
@@ -1,9 +1,27 @@
-const assert = require('chai');
+const assert = require('chai').assert;
 const { tileLabel, xyFromIndex } = require('../public/scripts/tileHelpers');
 
 
 describe('#tileLabel', () => {
   it('returns letter and number index for given xy coordinates', () => {
-    assert.equal(tileLabel((4, 5)), 'E4');
+    assert.equal(tileLabel(4, 5), 'F5');
+  });
+});
+
+describe('#xyFromIndex', () => {
+  it('returns (1, 1) from 5 in 3x3 grid', () => {
+    assert.deepEqual(xyFromIndex(5, 3), [1, 1]);
+  });
+
+  it('returns (4, 3) from 28 in 7x7 grid', () => {
+    assert.deepEqual(xyFromIndex(28, 7), [4, 3]);
+  });
+});
+
+describe('#xyFromIndex => #tileLabel', () => {
+  it('chaining of functions', () => {
+    const dim = 7;
+    const result = tileLabel(...xyFromIndex(28, dim), dim);
+    assert.equal(result, 'D5');
   });
 });
